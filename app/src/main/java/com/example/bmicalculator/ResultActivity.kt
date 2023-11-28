9afc647f724ac97cc80470eb796e49be05207754
kiwon kim <rlarldnjs17@gmail.com>
2023-11-28 14:05:41 +0900

    BMI 계산기

---
+++ app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -0,0 +1,66 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val height = intent.getIntExtra("height", 0)
+        val weight = intent.getIntExtra("weight", 0)
+
+        var value = weight / (height / 100.0).pow(2.0)
+        value = round(value*10) / 10
+
+        var resultText = ""
+        var resImage = 0
+        var resColor = 0
+
+        if (value < 18.5) {
+            resultText = "저체중"
+            resImage = R.drawable.img_lv1
+            resColor = Color.YELLOW
+        } else if (value >= 18.5 && value < 23.0) {
+            resultText = "정상제충"
+            resImage = R.drawable.img_lv2
+            resColor = Color.GREEN
+        } else if (value >= 23.0 && value < 25.0) {
+            resultText = "과체중"
+            resImage = R.drawable.img_lv3
+            resColor = Color.BLACK
+        } else if (value >= 25.0 && value < 30.0) {
+            resultText = "경도 비만"
+            resImage = R.drawable.img_lv4
+            resColor = Color.CYAN
+        } else if (value >= 30.0 && value < 35.0) {
+            resultText = "중정도 비만"
+            resImage = R.drawable.img_lv5
+            resColor = Color.MAGENTA
+        } else {
+            resultText = "고도비만"
+            resImage = R.drawable.img_lv6
+            resColor = Color.RED
+        }
+
+
+        val tv_resValue = findViewById<TextView>(R.id.tv_resValue)
+        val tv_resText = findViewById<TextView>(R.id.tv_resText)
+        val iv_resImage = findViewById<ImageView>(R.id.iv_image)
+        val btn_back = findViewById<Button>(R.id.btn_back)
+
+        tv_resValue.text = value.toString()
+        tv_resText.text = resultText
+
+        tv_resText.setTextColor(resColor)
+
+        iv_resImage.setImageResource(resImage)
+
+
+        btn_back.setOnClickListener{
+            finish()
+        }
+    }
+}
